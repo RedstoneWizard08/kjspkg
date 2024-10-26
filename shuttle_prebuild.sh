e1b52cae08edcb1a77e40e49437379739b717693
RedstoneWizard08 <jacob1coder@gmail.com>
2024-10-26 14:07:42 -0700

    libpq

--- shuttle_prebuild.sh
+++ shuttle_prebuild.sh
@@ -4,3 +4,5 @@ curl -fsSL https://bun.sh/install | bash
 [[ ! -d "/usr/local/bin" ]] && mkdir -p "/usr/local/bin"
 cp "$HOME/.bun/bin/bun" /usr/local/bin/bun
 cp "$HOME/.bun/bin/bunx" /usr/local/bin/bunx
+apt-get update
+apt-get -y install libpq-dev
