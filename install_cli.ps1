0293fb7810003bc94d5d515cca13151cc80d2dff
RedstoneWizard08 <94275204+RedstoneWizard08@users.noreply.github.com>
2024-11-17 14:14:32 -0800

    Create install_cli.ps1

---
+++ install_cli.ps1
@@ -0,0 +1,42 @@
+$ErrorActionPreference = "Stop"
+$tmpdir = $Env:TEMP
+$KJSPKG_VERSION = $Env:KJSPKG_VERSION
+
+if (-not $KJSPKG_VERSION) {
+    $KJSPKG_VERSION = 'latest'
+}
+
+$base_url = "https://github.com/RedstoneWizard08/kjspkg/releases/$KJSPKG_VERSION/download/kjspkg-"
+$proc_arch = [Environment]::GetEnvironmentVariable("PROCESSOR_ARCHITECTURE", [EnvironmentVariableTarget]::Machine)
+
+if ($proc_arch -eq "AMD64") {
+	$arch = "x86_64"
+} elseif ($proc_arch -eq "ARM64") {
+	$arch = "aarch64"
+} else {
+	Write-Host "Unsupported Architecture: $type" -ForegroundColor Red
+	exit 1
+}
+
+$url = "$base_url$arch-pc-windows-gnu.zip"
+$extract_path = "$tmpdir\kjspkg-cli"
+$zip_path = "$tmpdir\kjspkg-cli.zip"
+$local_bins = if ($Env:LOCAL_BINS -ne $null) { $Env:LOCAL_BINS } else { "$HOME\.local\bin" }
+
+if (!(Test-Path -Path "$local_bins" -PathType Container)) {
+	New-Item -ItemType Directory -Path "$local_bins" | Out-Null
+}
+
+Invoke-WebRequest "$url" -OutFile "$zip_path"
+Expand-Archive -Force "$zip_path" "$extract_path"
+Copy-Item -Path "$extract_path\kjspkg.exe" -Destination "$local_bins\kjspkg.exe" -Force
+
+if ($Env:Path -split ";" -notcontains "$local_bins") {
+    if (($Env:CI -ne $null) -and ($Env:GITHUB_PATH -ne $null)) {
+        Add-Content -Path "$Env:GITHUB_PATH" -Value "$local_bins"
+    } else {
+	    Write-Host ""
+    	Write-Host "Your path is missing $local_bins, you might want to add it." -ForegroundColor Red
+	    Write-Host ""
+     }
+}
